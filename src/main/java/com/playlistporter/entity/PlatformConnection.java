62c863db9f89ff267eead46db01666ea4eccfee1
Ansh Kataria <ansh.kataria@student.uq.edu.au>
2026-04-16 18:07:13 +1000

    add Platform Connection entity

--- src/main/java/com/playlistporter/entity/PlatformConnection.java
+++ src/main/java/com/playlistporter/entity/PlatformConnection.java
@@ -1,10 +1,20 @@
 package com.playlistporter.entity;
 
-import jakarta.annotation.Generated;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.Collate;
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
@@ -25,4 +35,21 @@ public class PlatformConnection {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable=false)
+    private Platform platform;
+
+    @Column(nullable = false)
+    private String accessToken;
+    private String refreshToken;
+
+    private LocalDateTime tokenExpiresAt;
+
+    private String platformUserId;
+    private String platformDisplayName;
 }
