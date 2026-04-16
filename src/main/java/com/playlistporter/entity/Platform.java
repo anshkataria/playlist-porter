3a81a1226e48a4bbc496faeea8a345fc0cf29ee4
Ansh Kataria <ansh.kataria@student.uq.edu.au>
2026-04-16 18:02:07 +1000

    add enum platform and wip: platform connection entity

---
+++ src/main/java/com/playlistporter/entity/Platform.java
@@ -0,0 +1,7 @@
+package com.playlistporter.entity;
+
+public enum Platform {
+    SPOTIFY,
+    APPLE_MUSIC,
+    YOUTUBE_MUSIC
+}
--- src/main/java/com/playlistporter/entity/PlatformConnection.java
+++ src/main/java/com/playlistporter/entity/PlatformConnection.java
@@ -1,5 +1,28 @@
 package com.playlistporter.entity;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "platform_connections", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id","platform"}))
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlatformConnection {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
