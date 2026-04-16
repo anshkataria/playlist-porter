1a35ef32da3af8698bdcfaa0ba128465d819576d
Ansh Kataria <ansh.kataria@student.uq.edu.au>
2026-04-16 20:51:37 +1000

    add track repository

---
+++ src/main/java/com/playlistporter/repository/TrackRepository.java
@@ -0,0 +1,6 @@
+package com.playlistporter.repository;
+import com.playlistporter.entity.Track;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrackRepository extends JpaRepository<Track, Long> {
+}
