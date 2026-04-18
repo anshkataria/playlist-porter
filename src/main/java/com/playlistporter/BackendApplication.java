efd80d8c939f5fe703e35f2674e73297f2de52a1
Ansh Kataria <ansh.kataria@student.uq.edu.au>
2026-04-18 14:58:32 +1000

    enable config properties in main class

--- src/main/java/com/playlistporter/BackendApplication.java
+++ src/main/java/com/playlistporter/BackendApplication.java
@@ -2,7 +2,12 @@ package com.playlistporter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.playlistporter.config.SpotifyConfig;
+import com.playlistporter.config.YouTubeConfig;
+
+@EnableConfigurationProperties({SpotifyConfig.class,YouTubeConfig.class})
 @SpringBootApplication
 public class BackendApplication {
 
